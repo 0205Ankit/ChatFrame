d44b7b2694f01b4639f8b4df4f912d9a07a1f6af
ankit <ankitbisht0205@gmail.com>
2024-02-14 23:03:38 +0530

    feat: initiating express server

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  pingTimeout: 60000, // what is a pong packet?
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server started on port 3000");
+});
--- src/app/queries.ts
+++ src/app/queries.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { getServerAuthSession } from "@/server/auth";
+import { Prisma } from "@prisma/client";
 
 export const getUserDetails = async () => {
   const session = await getServerAuthSession();
