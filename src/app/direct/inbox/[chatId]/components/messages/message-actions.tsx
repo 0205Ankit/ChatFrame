37ae1abeb3d672cc50fcc072e91f0fc1c27d84ed
ankit <ankitbisht0205@gmail.com>
2024-02-22 13:24:49 +0530

    feat: add ui for message actions

--- src/app/direct/inbox/[chatId]/components/messages/message-actions.tsx
+++ src/app/direct/inbox/[chatId]/components/messages/message-actions.tsx
@@ -1,7 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
+import { BsEmojiSmile } from "react-icons/bs";
+import { RxCopy } from "react-icons/rx";
+import { HiOutlineDotsVertical, HiOutlineReply } from "react-icons/hi";
+import { LuSend } from "react-icons/lu";
+import { cn } from "@/lib/utils";
+import { AiOutlineDelete } from "react-icons/ai";
 
-const MessageActions = () => {
-  return <div></div>;
+type PropType = React.HTMLAttributes<HTMLDivElement>;
+
+const MessageActions = ({ className }: PropType) => {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <Popover>
+        <PopoverTrigger>
+          <HiOutlineDotsVertical />
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] rounded-xl p-0 font-medium">
+          <p className="px-4 py-2 text-sm">Wed 11:30 AM</p>
+          <Separator />
+          <Button
+            variant={"noStyle"}
+            className="flex w-full items-center justify-between transition-all hover:bg-slate-200"
+          >
+            Forward <LuSend />
+          </Button>
+          <Button
+            variant={"noStyle"}
+            className="flex w-full items-center justify-between transition-all hover:bg-slate-200"
+          >
+            Copy <RxCopy />
+          </Button>
+          <Separator />
+          <Button
+            variant={"noStyle"}
+            className="flex w-full items-center justify-between text-error transition-all hover:bg-slate-200"
+          >
+            Delete <AiOutlineDelete />
+          </Button>
+        </PopoverContent>
+      </Popover>
+      <Button variant={"noStyle"} className="p-0">
+        <HiOutlineReply />
+      </Button>
+      <Button variant={"noStyle"} className="p-0">
+        <BsEmojiSmile />
+      </Button>
+    </div>
+  );
 };
 
 export default MessageActions;
--- src/app/direct/inbox/[chatId]/components/messages/message.tsx
+++ src/app/direct/inbox/[chatId]/components/messages/message.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useState } from "react";
+import MessageActions from "./message-actions";
 
 type PropType = React.HTMLAttributes<HTMLDivElement> & {
   text: string;
@@ -7,21 +9,31 @@ type PropType = React.HTMLAttributes<HTMLDivElement> & {
 };
 
 const Message = ({ text, isSender }: PropType) => {
+  const [showActions, setShowActions] = useState(false);
   return (
     <div
-      className={cn("relative mb-[2px] flex", {
+      className={cn("group mb-[2px] flex", {
         "justify-end": isSender,
       })}
+      onMouseEnter={() => setShowActions(true)}
+      onMouseLeave={() => setShowActions(false)}
     >
       <pre
         className={cn(
-          "w-fit rounded-[12px_12px_0_12px] bg-primary px-4 py-2 text-right font-sans font-medium text-white",
+          "relative w-fit max-w-[400px] rounded-[12px_12px_0_12px] bg-primary px-4 py-2 text-right font-sans font-medium text-white",
           {
             "rounded-[12px_12px_12px_0] bg-slate-300 text-black": !isSender,
           },
         )}
       >
         {text}
+        {isSender && showActions && (
+          <MessageActions
+            className={cn(
+              "absolute bottom-1/2 left-0 -translate-x-[calc(100%+10px)] translate-y-1/2 text-slate-700",
+            )}
+          />
+        )}
       </pre>
     </div>
   );
