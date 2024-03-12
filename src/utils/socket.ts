d2d93a26b91cc361742d12b54903b638bad64e65
ankit <ankitbisht0205@gmail.com>
2024-03-12 06:53:09 +0530

    fix: hardcode the server url in socket intialization

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
-const ENDPOINT = process.env.EXPRESS_SERVER_ENDPOINT ?? "";
+const ENDPOINT =
+  process.env.EXPRESS_SERVER_ENDPOINT ??
+  "https://chat-frame-server.vercel.app/"; /// TODO: don't hardcode the server url
 const socket = io(ENDPOINT);
 export default socket;
