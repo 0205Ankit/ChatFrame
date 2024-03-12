0cba55482a7dbb1acc0b2c2d70a7604bea777c35
ankit <ankitbisht0205@gmail.com>
2024-03-12 11:39:06 +0530

    fix: change express server url

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 // const ENDPOINT = process.env.EXPRESS_SERVER_ENDPOINT ?? "";
-const socket = io("https://chat-frame-server-three.vercel.app"); /// TODO: remove hard coded value
+const socket = io("https://chat-frame-server.vercel.app"); /// TODO: remove hard coded value
 export default socket;
