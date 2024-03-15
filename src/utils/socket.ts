f1a8e5a7a5c551d1366a4e57576b4a97fb7ba49c
ankit <ankitbisht0205@gmail.com>
2024-03-16 02:53:29 +0530

    patch: hardcode socket io url

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 const ENDPOINT = process.env.NEXT_PUBLIC_EXPRESS_SERVER_ENDPOINT ?? "";
-const socket = io(ENDPOINT);
+const socket = io("https://chat-frame-server-two.vercel.app/");
 export default socket;
