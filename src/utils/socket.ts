ea71852d90832b045f75fbc77d3bbc1b4a956b35
ankit <ankitbisht0205@gmail.com>
2024-03-16 00:47:55 +0530

    fix: get express server url from env variable

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-// const ENDPOINT = process.env.EXPRESS_SERVER_ENDPOINT ?? "";
-const socket = io("https://chat-frame-server.vercel.app"); /// TODO: remove hard coded value
+const ENDPOINT = process.env.NEXT_PUBLIC_EXPRESS_SERVER_ENDPOINT ?? "";
+const socket = io(ENDPOINT);
 export default socket;
