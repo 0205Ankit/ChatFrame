ae0330027bb9edf80d7bc3b0edd77fa71e0769a6
ankit <ankitbisht0205@gmail.com>
2024-03-16 10:46:42 +0530

    fix: add transports in socket initialization

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
 const ENDPOINT = process.env.NEXT_PUBLIC_EXPRESS_SERVER_ENDPOINT ?? "";
-const socket = io("https://chat-frame-server-two.vercel.app/");
+const socket = io(ENDPOINT, {
+  transports: ["websocket", "polling"],
+});
 export default socket;
